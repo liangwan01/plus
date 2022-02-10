015b6d9220f466e4730a902618ec6dd1783f8aec
kl <323233@qq.com>
2022-02-10 18:45:08 +0800

    direfjkdls;a

--- plus/src/main/java/com/wkcto/order/entity/Dept.java
+++ plus/src/main/java/com/wkcto/order/entity/Dept.java
@@ -14,6 +14,9 @@ import java.io.Serializable;
  */
 public class Dept implements Serializable {
 
+
+    //fdksl;a'kl
+
     private static final long serialVersionUID = 1L;
 
     @TableId(value = "id", type = IdType.ID_WORKER)
